66af0dde4cb25bf321885f2c0c9eb9fa5a3433fb
Kitty Davies <kittyfdavies@gmail.com>
2018-03-13 19:21:13 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 
 def batch_badge_creator(name_array)
   new_array = []
-  name.each_with_index { |name, index|
+  name_array.each_with_index { |name, index|
     room = index + 1
     new_array << "Hello, #{name}! You'll be assigned to room #{room}!"
   }
